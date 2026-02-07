ad380e61dcddfa1927c627eda6a1cf80d7ce1cd0
Rafsan Rad <rafsanrad@Rafsans-MacBook-Air.local>
2026-02-07 10:15:29 +0600

    added firebase and env file

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -4,6 +4,9 @@ import Home from "../pages/Home/Home/Home";
 import Coverage from "../pages/Coverage/Coverage";
 import ErrorPage from "../pages/Error/ErrorPage";
 import AboutUs from "../pages/About/AboutUs";
+import AuthLayout from "../layouts/AuthLayout";
+import Login from "../pages/Auth/Login/Login";
+import Register from "../pages/Auth/Register/Register";
 
 export const router = createBrowserRouter([
   {
@@ -24,6 +27,20 @@ export const router = createBrowserRouter([
         loader: () => fetch("/serviceCenters.json").then((res) => res.json()),
       },
     ],
+  },
+  {
+    path:'/',
+    Component:AuthLayout,
+    children:[
+      {
+        path:'login',
+        Component:Login
+      },
+      {
+        path:'register',
+        Component:Register
+      }
+    ]
   },
   {
     path: "*",
