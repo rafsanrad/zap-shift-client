8c83db72b50fdb024369917dc27e1af3e0ffee31
Rafsan Rad <rafsanrad@Rafsans-MacBook-Air.local>
2026-02-17 22:53:30 +0600

    added dashboard sidebar

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { TbTruckDelivery } from "react-icons/tb";
 import { Link, NavLink, Outlet } from "react-router";
 
 const DashboardLayout = () => {
   return (
-    <div className="drawer lg:drawer-open">
+    <div className="drawer lg:drawer-open max-w-7xl mx-auto">
       <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
       <div className="drawer-content">
         {/* Navbar */}
@@ -71,7 +72,14 @@ const DashboardLayout = () => {
 
             {/* our dashboard links */}
             <li>
-              <NavLink to='/dashboard/my-parcels'>My Parcels</NavLink>
+              <NavLink
+                className="is-drawer-close:tooltip is-drawer-close:tooltip-right"
+                data-tip="MyParcels"
+                to="/dashboard/my-parcels"
+              >
+                <TbTruckDelivery />
+                <span className="is-drawer-close:hidden">My Parcels</span>
+              </NavLink>
             </li>
 
             {/* List item */}
